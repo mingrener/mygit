5647a79b77b1fb060ae14acabd83ca85740951cc
xwp <xwp_fullstack@163.com>
2018-05-07 07:50:52 +0800

    add static web server

---
+++ learnPython/04-web服务器/01-static_web_server.py
@@ -0,0 +1,42 @@
+import re
+from socket import *
+from multiprocessing import Process
+
+
+def clTool(cl_socket,cl_addr):
+    #接受客户端发送的HTML报文信息
+    request_data = cl_socket.recv(1024)
+    request_data = request_data.decode("utf-8")
+    print("request data:\r\n", request_data)
+    # 解析报文数据
+    list_all_data = request_data.split("\r\n")
+    # 提取请求方式
+    #print("第一行%s"%list_all_data[0])
+    list_firt_line = re.split(r"[\s]+",list_all_data[0])
+    if "GET" in list_firt_line:
+        # 返回服务器页面信息
+        start_line_response = "HTTP/1.1 200 OK\r\n"
+        headers_response = "Server: My Server\r\n"
+        body_response = "hello world"
+        response = start_line_response + headers_response + "\r\n" + body_response
+        print("response data:\r\n",response)
+        cl_socket.send(response.encode("utf-8"))
+
+
+# 创建服务器端socket
+sv_socket = socket(AF_INET,SOCK_STREAM)
+sv_socket.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
+sv_addr = ("",6686)
+sv_socket.bind(sv_addr)
+sv_socket.listen(5)
+#print("----1----")
+while True:
+    #print("----2----")
+    # 获取已连接的客户端信息
+    cl_socket, cl_addr = sv_socket.accept()
+    print("客户端地址：",str(cl_addr))
+    cl_process = Process(target=clTool, args=(cl_socket,cl_addr))
+    cl_process.start()
+    cl_process.join()
+    cl_socket.close()
+
---
+++ learnPython/04-web服务器/02-static_web_server_file.py
@@ -0,0 +1,60 @@
+import re
+from socket import *
+from multiprocessing import Process
+# 设置静态文件根目录
+HMTL_ROOT_DIR = "./source"
+
+
+def clTool(cl_socket,cl_addr):
+    """处理客户端的请求"""
+    # 接受并打印客户端发送的报文信息
+    request_data = cl_socket.recv(1024)
+    request_data = request_data.decode("utf-8")
+    print("request data:\r\n", request_data)
+    # 解析报文数据
+    list_request_data = request_data.split("\r\n")
+    request_firt_line = re.split(r"[\s]+",list_request_data[0])
+    if "/" == request_firt_line[1]:
+        # 如果客户端没有文件路径，则指定默认文件路径
+        request_path = HMTL_ROOT_DIR + "/index.html"
+    else:
+        request_path = HMTL_ROOT_DIR + request_firt_line[1]
+    try:
+        # 读取文件中的数据
+        f = open(request_path, "rb")
+        file_data = f.read()
+        f.close()
+    except IOError:
+        # 如果文件路径不存在，返回404状态
+        start_line_response = "HTTP/1.1 404 Not Found\r\n"
+        headers_response = "Server: My Server\r\n"
+        body_response = "对不起，找不到该页面..."
+    else:
+        # 正常的相应数据
+        start_line_response = "HTTP/1.1 200 OK\r\n"
+        headers_response = "Server: My Server\r\n"
+        body_response = file_data.decode("utf-8")
+    # 返回响应数据
+    response = start_line_response + headers_response + "\r\n" + body_response
+    print("response data:\r\n",response)
+    cl_socket.send(response.encode("utf-8"))
+
+
+# 创建服务器端socket
+sv_socket = socket(AF_INET,SOCK_STREAM)
+sv_socket.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
+sv_addr = ("",6686)
+sv_socket.bind(sv_addr)
+sv_socket.listen(5)
+#print("----1----")
+while True:
+    #print("----2----")
+    # 获取已连接的客户端信息
+    cl_socket, cl_addr = sv_socket.accept()
+    print("客户端地址：",str(cl_addr))
+    cl_process = Process(target=clTool, args=(cl_socket,cl_addr))
+    cl_process.start()
+    cl_process.join()
+    cl_socket.close()
+
+
